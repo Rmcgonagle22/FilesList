f41b8fffee430d7550c6af34968daaaaf7ba9827
Rmcgonagle22 <113391575+Rmcgonagle22@users.noreply.github.com>
2022-09-19 20:30:48 -0400

    FilesList.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 my_file=open("requiredCS.txt", "r")
 all_lines=my_file.readlines()
-print(all_lines)
+for line in all_lines:
+    upper_case_line = line.upper()
+    print(upper_case_line)
+print("Those are the required COMP courses for the major")
+
